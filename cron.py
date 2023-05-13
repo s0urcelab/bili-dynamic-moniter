f7cc9065b83cfbf1ee0d0f3bbcf5025050eb8875
s0urce <me@src.moe>
2023-05-13 13:15:29 +0800

    test code 4

--- cron.py
+++ cron.py
@@ -13,8 +13,8 @@ logger = logging.getLogger('bdm')
 
 if __name__ == '__main__':
     scheduler = AsyncIOScheduler(timezone='Asia/Shanghai')
-    scheduler.add_job(update, 'interval', minutes=10)
+    scheduler.add_job(update, 'interval', minutes=5)
     scheduler.add_job(match, 'interval', minutes=10)
-    scheduler.add_job(download, 'interval', minutes=15)
+    scheduler.add_job(download, 'interval', minutes=10)
     scheduler.start()
     asyncio.get_event_loop().run_forever()
